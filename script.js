0f303df7a9d86e32977b208c20bb9bc344e38b42
Giacomo <giacomo@MacBook-Pro-di-Giacomo.local>
2020-11-17 03:58:15 +0100

    Ok

--- script.js
+++
@@ -1,9 +0,0 @@
-var toglemenu = document.querySelector(".togle-menu")
-var sidebar = document.querySelector(".side-bar")
-var mainheader = document.querySelector(".main-header")
-
-toglemenu.addEventListener("click", function() {
-    sidebar.classList.toggle("hide");
-    mainheader.classList.toggle("color")
-
-})
